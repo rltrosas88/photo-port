5c5147ea50761661a797bbd24451a9da6213f1b0
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-24 13:57:37 -0500

    20.5.5

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+const Modal = ({ onClose, currentPhoto }) => {
     const {name, category, description, index} = currentPhoto;
 
     return (
@@ -10,10 +10,8 @@ function Modal({currentPhoto}) {
             <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
             <p>
                 {description}
-                </p>
-            <button type="button">
-                Close this modal
-            </button>
+            </p>
+            <button onClick={onClose} type="button">Close this modal</button>
         </div>
         </div>
     );
