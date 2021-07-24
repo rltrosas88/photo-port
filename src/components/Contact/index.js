f3d2096fe8318b1902b0bf0edb7a1a4f3a121d7b
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-24 11:27:19 -0500

    20.4.5 Validate the form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-    const { name, email, message } = formState;
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
-      
-    // console.log(formState);
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
     }
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        } 
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+      
+    // console.log(formState);
+
+
 
     return (
         <section>
